518f2918d5b6cbea39c3be635d9235b38979a17f
nina <muhikaninina@gmail.com>
2024-04-16 07:33:04 +0300

    review scope

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution in this file!
+var customerName ='bob';
+var bestCustomer;
+const leastFavoriteCustomer = 'tom'
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase(); 
+    return customerName;
+}
+function setBestCustomer(){
+   return bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob';
+}
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = 'John';
+}
